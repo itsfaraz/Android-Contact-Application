9d8c20e598614431b688d4eb0087bca42039a68d
itsfaraz <faraz.abdul@anywhere.co>
2022-09-08 15:04:51 +0530

    class mapper added

---
+++ app/src/main/java/com/itsfrz/authentication/data/entities/mapper/ContactModelMapper.kt
@@ -0,0 +1,52 @@
+package com.itsfrz.authentication.data.entities.mapper
+
+import androidx.room.Transaction
+import com.itsfrz.authentication.data.entities.ContactModel
+import com.itsfrz.support.Contact
+
+
+object ContactModelMapper{
+
+    @Transaction
+    fun transformContactToContactModel(contact : Contact) : ContactModel{
+        return ContactModel(
+            contactId = contact.contactId,
+            contactName = contact.contactName,
+            contactNumber = contact.contactNumber,
+            hasContactImage = getImageBoolean(contact.contactThumbnailImage),
+            contactImage = contact.contactImage,
+            contactAddress = contact.contactAddress,
+            contactEmailId = contact.contactEmailId,
+            contactCountry = contact.contactCountry,
+            contactPostCode = contact.contactPostalCode,
+            username = "",
+        )
+    }
+
+
+    private fun getImageBoolean(contactThumbnailImage: String): Boolean =
+        contactThumbnailImage.isNotBlank() && contactThumbnailImage.isNotEmpty()
+
+
+    @Transaction
+    fun transformContactModelToContact(contact : ContactModel) : Contact{
+        return Contact(
+            contactId = contact.contactId,
+            contactName = contact.contactName,
+            contactNumber = contact.contactNumber,
+            contactImage = "",
+            contactThumbnailImage = contact.contactImage,
+            contactOrganization = "",
+            contactJobTitle = "",
+            contactAddress = contact.contactAddress,
+            contactWebAddress = "",
+            contactEmailId = contact.contactEmailId,
+            contactCountry = contact.contactCountry,
+            contactPostalCode = contact.contactPostCode
+        )
+    }
+
+
+
+
+}
