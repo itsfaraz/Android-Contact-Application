f04351cda0b20f315d6d3fead284668fc54ad798
itsfaraz <faraz.abdul@anywhere.co>
2022-09-15 19:29:32 +0530

    future use deep link added

---
+++ app/src/main/java/com/itsfrz/authentication/ui/utils/ContactDeepLink.kt
@@ -0,0 +1,12 @@
+package com.itsfrz.authentication.ui.utils
+
+object ContactDeepLink {
+    private const val DOMAIN = "android-app://itsfrz.authentication"
+
+    private const val AUTHENTICATION = "$DOMAIN/authentication"
+    private const val PERMISSION = "$DOMAIN/permission"
+
+
+
+
+}
