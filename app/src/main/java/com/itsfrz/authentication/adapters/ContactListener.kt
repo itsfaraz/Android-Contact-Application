f4b5578759b55bd429f362ed23bb6fbcfaa9a580
itsfaraz <faraz.abdul@anywhere.co>
2022-09-17 15:36:16 +0530

    unknown file removed

--- app/src/main/java/com/itsfrz/authentication/adapters/ContactListener.kt
+++
@@ -1,7 +0,0 @@
-package com.itsfrz.authentication.adapters
-
-import com.itsfrz.authentication.data.entities.ContactModel
-
-interface ContactListener {
-    public fun onContactChange(list : List<ContactModel>) : Unit
-}
